a6d2b0fa052afec847e062a3b93e92a79430e10b
William Tavares <williamtavaresdemoura@gmail.com>
2024-05-31 15:37:53 -0300

    Inserindo dados

--- conexao.py
+++ conexao.py
@@ -1,9 +1,20 @@
 import sqlite3
 from pathlib import Path
 
+# Criando o caminho para o arquivo, do sqlite
+
 ROOT_PATH = Path(__file__).parent
 
-
+# Criando banco de dados sqlite com o ROOT_PATH
 conexao = sqlite3.connect(ROOT_PATH / "clientes.sqlite")
+
+# Usando o cursor para usar os comandos sql
 cursor = conexao.cursor()
-cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+
+# Usando o .execute para criar a tabela clientes dentro do banco de dados clientes
+# cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+
+data = ("William", "will@gmail.com")
+# obs: NA HORA DE INSERIR DADOS É UMA BOA PRATICA DE SEGURANÇA 
+cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?);", data )
+conexao.commit()
